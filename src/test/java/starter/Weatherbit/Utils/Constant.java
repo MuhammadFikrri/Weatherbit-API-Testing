6196f4b7a590824e9386ddf61cc948d0b89c29e5
MuhammadFikrri <muhammadfikrri7@gmail.com>
2023-04-29 11:24:24 +0700

    update token

--- src/test/java/starter/Weatherbit/Utils/Constant.java
+++ src/test/java/starter/Weatherbit/Utils/Constant.java
@@ -2,7 +2,7 @@ package starter.Weatherbit.Utils;
 
 public class Constant
 {
-    public static final String WEATHER_API_KEY = "03b64cff034844769e74cc85eb62e2fd";
-    public static String BASE_URL = "https://api.weatherbit.io/v2.0/current?lat=40.730610&lon=-73.935242&key=03b64cff034844769e74cc85eb62e2fd";
-    public static String BASE_URL_FORECAST = "https://api.weatherbit.io/v2.0/forecast/hourly?postal_code=28546&hours=3&key=03b64cff034844769e74cc85eb62e2fd";
+    public static final String WEATHER_API_KEY = "f31e5b7ebb9a49368902f606f5ae5e96";
+    public static String BASE_URL = "https://api.weatherbit.io/v2.0/current?lat=40.730610&lon=-73.935242&key=f31e5b7ebb9a49368902f606f5ae5e96";
+    public static String BASE_URL_FORECAST = "https://api.weatherbit.io/v2.0/forecast/hourly?postal_code=28546&hours=3&key=f31e5b7ebb9a49368902f606f5ae5e96";
 }
